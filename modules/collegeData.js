9f45d561f0f245af0f682bdf25401d2e82857702
Pmalinnag <PMlinnag039319@gmail.com>
2022-07-10 11:31:02 -0400

    assignment 3 update

---
+++ modules/collegeData.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+
+class Data{
+    constructor(students, courses){
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+module.exports.initialize = function () {
+    return new Promise( (resolve, reject) => {
+        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+            if (err) {
+                reject("unable to load courses"); return;
+            }
+
+            fs.readFile('./data/students.json','utf8', (err, studentData) => {
+                if (err) {
+                    reject("unable to load students"); return;
+                }
+
+                dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
+                resolve();
+            });
+        });
+    });
+}
+
+module.exports.getAllStudents = function(){
+    return new Promise((resolve,reject)=>{
+        if (dataCollection.students.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(dataCollection.students);
+    })
+}
+
+module.exports.getTAs = function () {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].TA == true) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
+        }
+
+        if (filteredStudents.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(filteredStudents);
+    });
+};
+
+module.exports.getCourses = function(){
+   return new Promise((resolve,reject)=>{
+    if (dataCollection.courses.length == 0) {
+        reject("query returned 0 results"); return;
+    }
+
+    resolve(dataCollection.courses);
+   });
+};
+
+module.exports.getStudentByNum = function (num) {
+    return new Promise(function (resolve, reject) {
+        var foundStudent = null;
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].studentNum == num) {
+                foundStudent = dataCollection.students[i];
+            }
+        }
+
+        if (!foundStudent) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(foundStudent);
+    });
+};
+
+module.exports.getStudentsByCourse = function (course) {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].course == course) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
+        }
+
+        if (filteredStudents.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(filteredStudents);
+    });
+};
+
---
+++ server.js
@@ -0,0 +1,124 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Precious Marie Malinnag        
+*  Student ID: 147010219 
+*  Date: 16 June 2022
+*
+********************************************************************************/ 
+
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+const path = require("path")
+var app = express();
+
+var collegedata = require('./modules/collegeData')
+
+
+// Add middleware for static contents
+app.use(express.static('views'))
+app.use(express.static('modules'))
+
+// Get request for student details
+app.get('/students', (req, res) => {
+
+    if( req.query.course &&  req.query.course !== undefined){
+        let courseFromParams = req.query.course;
+        console.log(courseFromParams);
+
+        collegedata.initialize().then(data => {
+            collegedata.getStudentsByCourse(courseFromParams).then(course => {
+                res.send(course)
+                console.log("courses data retrieved")
+            }).catch()
+           
+        }).catch(err => {
+            console.log(err)
+        })
+    } else {
+        collegedata.initialize().then(data => {
+            collegedata.getAllStudents().then(students => {
+            res.send(students)
+            console.log("students suceeded")
+            }).catch()
+        }).catch(err => {
+            err = {
+                message : "no results"}
+            res.send()
+        })
+    }
+})
+
+app.get("/tas", (req, res) => {
+    collegedata.initialize().then(data => {
+        collegedata.getTAs().then(tas => {
+        res.send(tas)
+        console.log("tas suceeded" )
+        }).catch()
+    }).catch(err => {
+        err = {
+            message : "no results"}
+        res.send()
+    })
+})
+
+app.get("/courses", (req, res) => {
+    console.log("Entering courses")
+    collegedata.initialize().then(data => {
+        collegedata.getCourses().then(courses => {
+        res.send(courses)
+        console.log("courses suceeded" )
+        }).catch()
+    }).catch(err => {
+        err = {
+            message : "no results"}
+        res.send()
+    })
+})
+
+app.get("/student/:studentnum", (req, res) => {
+    console.log("Entering student num")
+    let studentnumber = req.params.studentnum
+        collegedata.initialize().then(data => {
+            collegedata.getStudentByNum(studentnumber).then(students => {
+                res.send(students)
+                console.log("student data retrieved")
+            }).catch(err => {
+                console.log(err)
+            })
+           
+        }).catch(err => {
+            console.log(err)
+        })
+})
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+});
+
+
+// setup a 'route' to listen on the default url path
+app.get("/htmlDemo", (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/htmlDemo.html"));
+});
+
+app.get('*', function(req, res){
+    res.send('Page Not Found', 404);
+});
+
+
+// setup http server to listen on HTTP_PORT
+collegedata.initialize()
+.then(app.listen(HTTP_PORT, ()=>{
+    
+    console.log("server listening on port: " + HTTP_PORT)
+}))
+.catch(err => {
+    console.log("Error in intializing with the json files")
+})
+
