f08b33478a383cb6761f524f9a9b9b05a5037d6d
PMarie <PMalinnag039319@gmail.com>
2022-07-23 19:39:09 -0400

    Assign 5 express handlebars update

--- server.js
+++ server.js
@@ -11,19 +11,60 @@
 *
 ********************************************************************************/ 
 
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-const path = require("path")
-var app = express();
+// var HTTP_PORT = process.env.PORT || 8080;
+//var express = require("express");
+//const path = require("path")
+//const exphbs = require('express-handlebars'); //assign 5 update
+//var app = express();
 
 var collegedata = require('./modules/collegeData')
-
 
 // Add middleware for static contents
 app.use(express.static('views'))
 app.use(express.static('modules'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
+//assign 5 update starts here
+// setup our requires 
+const HTTP_PORT = 8080;
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// Register handlebars as the rendering engine for views
+app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+app.get("/viewData", function(req,res){
+
+    var someData = {
+        name: "John",
+        age: 23,
+        occupation: "developer",
+        company: "Scotiabank"
+    };
+
+    res.render('viewData', {
+        data: someData,
+        layout: false // do not use the default Layout (main.hbs)
+    });
+
+});
+
+// start the server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
+//assign 5 update ends here
+    res.render('viewData', {
+        data: someData,
+        layout: false // do not use the default Layout (main.hbs)
+    });
 
 // Step Get details for students, tas, courses, student num:
 app.get('/students', (req, res) => {
