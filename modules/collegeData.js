4025ecf76128db64692238a3babea4b1370b8d8c
PMarie <PMalinnag039319@gmail.com>
2022-08-07 16:45:01 -0400

    A6 retracing ... 4

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -1,6 +1,8 @@
+
+
 const Sequalize = require("Sequelize");
 
-var Sequelize = new Sequelize("database", "user", "password", {
+const Sequelize = new Sequelize.define("database", "user", "password", { //this is where the error is traced, professor please help
     host: "gost", dialect: "postgres", port: 5432, dialectOptions: {
       ssl: { rejectUnauthorized: false },
     },
@@ -31,8 +33,99 @@ var Course = Sequelize.define("Course", {
     courseCode: Sequelize.STRING,
     courseDescription: Sequelize.STRING,
 });
-  
-  Course.hasMany(Student, { foreignKey: "course" });
+
+module.exports.initialize = function () {
+    return new Promise( (resolve, reject) => {
+        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+            if (err) {
+                reject("unable to load courses"); return;
+            }
+
+            fs.readFile('./data/students.json','utf8', (err, studentData) => {
+                if (err) {
+                    reject("unable to load students"); return;
+                }
+
+                dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
+                resolve();
+            });
+        });
+    });
+}
+
+module.exports.getAllStudents = function(){
+    return new Promise((resolve,reject)=>{
+        if (dataCollection.students.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(dataCollection.students);
+    })
+}
+
+module.exports.getTAs = function () {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].TA == true) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
+        }
+
+        if (filteredStudents.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(filteredStudents);
+    });
+};
+
+module.exports.getCourses = function(){
+   return new Promise((resolve,reject)=>{
+    if (dataCollection.courses.length == 0) {
+        reject("query returned 0 results"); return;
+    }
+
+    resolve(dataCollection.courses);
+   });
+};
+
+module.exports.getStudentByNum = function (num) {
+    return new Promise(function (resolve, reject) {
+        var foundStudent = null;
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].studentNum == num) {
+                foundStudent = dataCollection.students[i];
+            }
+        }
+
+        if (!foundStudent) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(foundStudent);
+    });
+};
+
+module.exports.getStudentsByCourse = function (course) {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].course == course) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
+        }
+
+        if (filteredStudents.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(filteredStudents);
+    });
+};
+Course.hasMany(Student, { foreignKey: "course" });
 function initialize() {
     return new Promise(function (resolve, reject) {
       sequelize
@@ -201,97 +294,7 @@ function addCourse(course) {
   
 let dataCollection = null;
 
-module.exports.initialize = function () {
-    return new Promise( (resolve, reject) => {
-        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
-            if (err) {
-                reject("unable to load courses"); return;
-            }
 
-            fs.readFile('./data/students.json','utf8', (err, studentData) => {
-                if (err) {
-                    reject("unable to load students"); return;
-                }
-
-                dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
-                resolve();
-            });
-        });
-    });
-}
-
-module.exports.getAllStudents = function(){
-    return new Promise((resolve,reject)=>{
-        if (dataCollection.students.length == 0) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(dataCollection.students);
-    })
-}
-
-module.exports.getTAs = function () {
-    return new Promise(function (resolve, reject) {
-        var filteredStudents = [];
-
-        for (let i = 0; i < dataCollection.students.length; i++) {
-            if (dataCollection.students[i].TA == true) {
-                filteredStudents.push(dataCollection.students[i]);
-            }
-        }
-
-        if (filteredStudents.length == 0) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(filteredStudents);
-    });
-};
-
-module.exports.getCourses = function(){
-   return new Promise((resolve,reject)=>{
-    if (dataCollection.courses.length == 0) {
-        reject("query returned 0 results"); return;
-    }
-
-    resolve(dataCollection.courses);
-   });
-};
-
-module.exports.getStudentByNum = function (num) {
-    return new Promise(function (resolve, reject) {
-        var foundStudent = null;
-        for (let i = 0; i < dataCollection.students.length; i++) {
-            if (dataCollection.students[i].studentNum == num) {
-                foundStudent = dataCollection.students[i];
-            }
-        }
-
-        if (!foundStudent) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(foundStudent);
-    });
-};
-
-module.exports.getStudentsByCourse = function (course) {
-    return new Promise(function (resolve, reject) {
-        var filteredStudents = [];
-
-        for (let i = 0; i < dataCollection.students.length; i++) {
-            if (dataCollection.students[i].course == course) {
-                filteredStudents.push(dataCollection.students[i]);
-            }
-        }
-
-        if (filteredStudents.length == 0) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(filteredStudents);
-    });
-};
 module.exports = {
     initialize,
     getAllStudents,
